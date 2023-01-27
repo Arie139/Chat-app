945d2e0fa9c58f3165c0068bdd61603512c2b353
Ariel <90644357+Arie139@users.noreply.github.com>
2023-01-26 19:32:16 -0500

    log in and out component

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAeaaaoGMkEwKrxxOIO7pNIe7jSkqeKbbQ",
+  authDomain: "chat-app-ca771.firebaseapp.com",
+  projectId: "chat-app-ca771",
+  storageBucket: "chat-app-ca771.appspot.com",
+  messagingSenderId: "672175443652",
+  appId: "1:672175443652:web:5a8c37c6448f02f8fa8391"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
